0f15f2e19b47270b6e0643589a45bd1487afd852
SUNDHAR2402 <139101416+SUNDHAR2402@users.noreply.github.com>
2023-07-12 10:56:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+function bulb_on()
+{
+    img.src="./images/download.jpg";
+}
+function bulb_off()
+{
+    img.src="./images/images.jpg";
+}
